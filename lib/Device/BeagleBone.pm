02ad3a6f131a8fec963adfcb024b51fe7724bd7f
jheddings <jason@heddings.com>
2015-10-19 15:13:20 -0600

    Initial makefile support.

---
+++ lib/Device/BeagleBone.pm
@@ -0,0 +1,37 @@
+package Device::BeagleBone;
+
+use 5.018002;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;  ## EOM
+__END__
+
+=head1 NAME
+
+Device::BeagleBone - Perl library for working with the BeagleBone family.
+
+=head1 SYNOPSIS
+
+  use Device::BeagleBone::GPIO;
+  use Device::BeagleBone::LEDS;
+
+=head1 DESCRIPTION
+
+TODO
+
+=head1 AUTHOR
+
+Jason Heddings, E<lt>jason@heddings.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2015 by Jason Heddings
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.18.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut

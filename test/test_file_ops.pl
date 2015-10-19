7ef00e56fca0789950f85daeda0fc7474d0e4ae8
jheddings <jason@heddings.com>
2015-10-18 18:28:57 -0600

    Unit test improvements for BlackBone::File.

--- test/test_file_ops.pl
+++ test/test_file_ops.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use Test::Simple tests => 5;
+use Test::Simple tests => 6;
 
 # XXX this test must be run from the test directory
 
@@ -11,17 +11,20 @@ use lib '../source';
 use BlackBone::File;
 
 my $basic_test_data = BlackBone::File::read_file('data/basic_text');
-ok($basic_test_data eq "basic test\n");
+ok($basic_test_data eq "basic text\n", 'basic read test');
+
+my $multiline_test_data = BlackBone::File::read_file('data/multiline_text');
+ok($multiline_test_data eq "multiline\ntext\n", 'multiline read test');
 
 my $int_test_data = BlackBone::File::read_int('data/integer');
-ok($int_test_data eq 42);
+ok($int_test_data eq 42, 'read single integer from file');
 
 my $expr_dat1 = BlackBone::File::read_expr('data/mixed_data', qr/"(.*)"/);
-ok($expr_dat1 eq 'test data');
+ok($expr_dat1 eq 'test data', 'read expression on first line');
 
 my $expr_dat2 = BlackBone::File::read_int('data/mixed_data');
-ok($expr_dat2 eq 24);
+ok($expr_dat2 eq 24, 'read single integer from mixed text');
 
 my $expr_dat3 = BlackBone::File::read_expr('data/mixed_data', qr/@([^ ]+)/);
-ok($expr_dat3 eq 'there');
+ok($expr_dat3 eq 'there', 'read expression from mixed text');
 

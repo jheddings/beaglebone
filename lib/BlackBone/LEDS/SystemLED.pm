ba63c256d51592c367a6f23f8d8306ebb57be0cf
jheddings <jason@heddings.com>
2015-10-18 20:40:24 -0600

    Removed catfile: it was messy to look at

--- lib/BlackBone/LEDS/SystemLED.pm
+++ lib/BlackBone/LEDS/SystemLED.pm
@@ -4,7 +4,6 @@ use base qw(BlackBone::LEDS::LED);
 use strict;
 use warnings;
 
-use File::Spec;
 use File::Basename;
 use BlackBone::File;
 
@@ -21,12 +20,12 @@ sub all {
 sub get {
   my ($name) = @_;
 
-  my $path = File::Spec->catfile($LEDS_SYS_PATH, $name);
+  my $sysroot = "$LEDS_SYS_PATH/$name";
 
   # TODO better error handling
-  -e $path or die;
+  -e $sysroot or die;
 
-  return new BlackBone::LEDS::SystemLED($path);
+  return new BlackBone::LEDS::SystemLED($sysroot);
 }
 
 ################################################################################
@@ -66,15 +65,15 @@ sub blink {
   # we need to set the trigger to ensure the delay_X entries appear
   $self->trigger('timer');
 
-  BlackBone::File::write_file($self->syspath('delay_on'), $on_ms);
-  BlackBone::File::write_file($self->syspath('delay_off'), $off_ms);
+  BlackBone::File::write_file($self->{sysroot} . '/delay_on', $on_ms);
+  BlackBone::File::write_file($self->{sysroot} . '/delay_off', $off_ms);
 }
 
 ################################################################################
 sub max_bright {
   my $self = shift;
 
-  my $syspath = $self->syspath('max_brightness');
+  my $syspath = $self->{sysroot} . '/max_brightness';
 
   return BlackBone::File::read_int($syspath);
 }
@@ -83,7 +82,7 @@ sub max_bright {
 sub brightness {
   my ($self, $bright) = @_;
 
-  my $syspath = $self->syspath('brightness');
+  my $syspath = $self->{sysroot} . '/brightness';
 
   # if the user set the brightness, write it here
   if (defined $bright and length $bright) {
@@ -97,7 +96,7 @@ sub brightness {
 sub trigger {
   my ($self, $trigger) = @_;
 
-  my $syspath = $self->syspath('trigger');
+  my $syspath = $self->{sysroot} . '/trigger';
 
   # if the user set the trigger, write it here
   if ($trigger) {
@@ -106,13 +105,6 @@ sub trigger {
 
   # the trigger entry has brackets around the current value
   return BlackBone::File::read_expr($syspath, qr/\[(.*)\]/);
-}
-
-################################################################################
-sub syspath {
-  my ($self, $name) = @_;
-
-  return File::Spec->catfile($self->{sysroot}, $name);
 }
 
 ################################################################################
